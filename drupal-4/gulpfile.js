3ea0a7ed30a769299cc0b4396a3a5de878b738e1
eardles <john@eardley.net>
2015-12-05 10:55:53 +0000

    The gulpfile now handles multiple sass imports withouth error.

--- drupal-4/gulpfile.js
+++ drupal-4/gulpfile.js
@@ -1,48 +1,36 @@
 var gulp = require('gulp');
-var livereload = require('gulp-livereload')
-var uglify = require('gulp-uglifyjs');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var runSequence = require('run-sequence');
 
+gulp.task('sass', function() {
+  return gulp.src('./themes/custom/endymion/sass/*.scss') 
+  .pipe(sass())
+    .pipe(gulp.dest('./themes/custom/endymion/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
 
-
-
-gulp.task('imagemin', function () {
-    return gulp.src('./themes/custom/endymion/images/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./themes/custom/endymion/images'));
-});
-
-
-gulp.task('sass', function () {
-  gulp.src('./themes/custom/endymion/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./themes/custom/endymion/css'));
-});
-
-
-gulp.task('uglify', function() {
-  gulp.src('./themes/custom/endymion/lib/*.js')
-    .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./themes/custom/endymion/js'))
-});
-
-gulp.task('watch', function(){
-    livereload.listen();
-
-    gulp.watch('./themes/custom/endymion/sass/**/*.scss', ['sass']);
-    gulp.watch('./themes/custom/endymion/lib/*.js', ['uglify']);
-    gulp.watch(['./themes/custom/endymion/css/style.css', './themes/custom/endymion/**/*.twig', './themes/custom/endymion/js/*.js'], function (files){
-        livereload.changed(files)
+gulp.task('browserSync', function() {
+    browserSync.init({
+        proxy: "localhost:85/drupal-4"
     });
+})
+
+gulp.task('clean',function(){
+    del(['./themes/custom/endymion/sass/*.css']);
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('./themes/custom/endymion/sass/*.scss', ['sass', 'clean']);
+
+  // Reloads the browser whenever HTML or JS files change
+  //gulp.watch('*.html', browserSync.reload); 
+  //gulp.watch('js/**/*.js', browserSync.reload); 
+})
+
+gulp.task('default', function(callback) {
+  runSequence('clean', 'watch', callback);
 });
